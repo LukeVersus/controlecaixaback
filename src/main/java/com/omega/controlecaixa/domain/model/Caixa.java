44f05dbb96471b82939100ce156f911b6c5bbef6
Lucas Ramos <lvcramos@hotmail.com>
2025-03-15 15:57:08 -0300

    feat(controlecaixa): Adição de model e repository de caixa

---
+++ src/main/java/com/omega/controlecaixa/domain/model/Caixa.java
@@ -0,0 +1,23 @@
+package com.omega.controlecaixa.domain.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "caixa", schema = "omega")
+@Data
+public class Caixa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_caixa")
+    @SequenceGenerator(name = "seq_caixa", sequenceName = "seq_caixa", schema = "omega", allocationSize = 1)
+    private Long id;
+
+    @NotEmpty(message = "Descrição é obrigatória.")
+    private String descricao;
+
+    private BigDecimal saldoInicial = BigDecimal.ZERO;
+}
---
+++ src/main/java/com/omega/controlecaixa/repositories/CaixaRepository.java
@@ -0,0 +1,8 @@
+package com.omega.controlecaixa.repositories;
+
+import com.omega.controlecaixa.domain.model.Caixa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CaixaRepository extends JpaRepository<Caixa, Long> {
+
+}
