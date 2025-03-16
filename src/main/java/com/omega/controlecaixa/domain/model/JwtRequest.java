a2e7242e8862818c9fc1bbd9f7288cd81c99fa05
Lucas Ramos <lvcramos@hotmail.com>
2025-03-16 00:06:59 -0300

    fix(controlecaixa): Adição de annotation all args e no args constructors.

--- src/main/java/com/omega/controlecaixa/domain/model/JwtRequest.java
+++ src/main/java/com/omega/controlecaixa/domain/model/JwtRequest.java
@@ -1,8 +1,12 @@
 package com.omega.controlecaixa.domain.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class JwtRequest {
     private String username;
     private String password;
