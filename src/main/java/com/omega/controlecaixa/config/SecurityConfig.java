a57511aebc3267756134daba7ed4c9ef753db795
Lucas Ramos <lvcramos@hotmail.com>
2025-03-15 21:28:29 -0300

    feat(controlecaixa): Configuração do swagger.

--- src/main/java/com/omega/controlecaixa/config/SecurityConfig.java
+++ src/main/java/com/omega/controlecaixa/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/login").permitAll()
                         .requestMatchers("/api/**").authenticated()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().permitAll()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
