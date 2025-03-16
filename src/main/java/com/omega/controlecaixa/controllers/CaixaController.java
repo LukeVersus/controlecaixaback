fe9e6150489b3586f6920983e9a438deef6fc67a
Lucas Ramos <lvcramos@hotmail.com>
2025-03-15 21:28:15 -0300

    feat(controlecaixa): Criado controller de caixa.

--- src/main/java/com/omega/controlecaixa/controllers/CaixaController.java
+++ src/main/java/com/omega/controlecaixa/controllers/CaixaController.java
@@ -4,11 +4,9 @@ import com.omega.controlecaixa.domain.model.Caixa;
 import com.omega.controlecaixa.domain.service.interfaces.CaixaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
@@ -26,6 +24,28 @@ public class CaixaController {
     @GetMapping("{id}")
     ResponseEntity<Caixa> recuperarCaixa(@PathVariable("id") Long id) {
         return ResponseEntity.ok().body(service.recuperarCaixa(id));
+    }
+
+    @PostMapping
+    ResponseEntity<Caixa> salvarCaixa(@RequestBody Caixa caixa) {
+        return ResponseEntity.ok().body(service.salvarCaixa(caixa));
+    }
+
+    @PutMapping("{id}/saldo-inicial")
+    ResponseEntity<Caixa> alterarSaldoInicial(@PathVariable("id") Long id, @RequestBody BigDecimal saldoInicial) {
+        return ResponseEntity.ok().body(service.alterarSaldoInicial(id, saldoInicial));
+    }
+
+    @PutMapping("{id}")
+    ResponseEntity<String> alterarCaixa(@PathVariable("id") Long id, @RequestBody Caixa caixa) {
+        service.alterarCaixa(id, caixa);
+        return ResponseEntity.ok().body("Alterado com sucesso.");
+    }
+
+    @DeleteMapping("{id}")
+    ResponseEntity<String> excluirCaixa(@PathVariable("id") Long id) {
+        service.excluirCaixa(id);
+        return ResponseEntity.ok().body("Excluído com sucesso.");
     }
 
 }
